ef7096fbab4bb7e40d23971310ef41bbf83a82c8
Naroz Ezzat <narozezzat10@gmail.com>
2025-05-08 02:41:40 +0300

    fix title of header

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[500px] '>
             <Image
-                src='https://images.unsplash.com/photo-1562832135-14a35d25edef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1245&q=80'
+                src='/banner.jpg'
                 alt='banner-img'
                 className='object-cover object-left'
                 fill
